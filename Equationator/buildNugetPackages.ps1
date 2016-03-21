4fe29426ff59fe53ec6b26a2bd21c0e97bc605c8
Dan Manning <dmanning23@hotmail.com>
2016-03-20 23:05:21 -0500

    android native lib

--- Equationator/buildNugetPackages.ps1
+++ Equationator/buildNugetPackages.ps1
@@ -1,4 +1,3 @@
 nuget pack .\Equationator\Equationator.nuspec -IncludeReferencedProjects -Prop Configuration=Release
-
-
 nuget pack .\Equationator.PCL\Equationator.PCL.nuspec -IncludeReferencedProjects -Prop Configuration=Release
+nuget pack .\Equationator.Android\Equationator.Android.nuspec -IncludeReferencedProjects -Prop Configuration=Release
